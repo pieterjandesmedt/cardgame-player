31ae867fd42afd8d74dab3eaf9d8c5acc8aabebd
Pieter Jan De Smedt <pieterjan@digitalcomet.be>
2021-03-01 12:08:15 +0100

    fix: keep trying for ws solution

--- src/store/index.js
+++ src/store/index.js
@@ -9,8 +9,7 @@ import { CardGame } from '../game.js';
 
 const server =
 	process.env.NODE_ENV === 'production' ? 'https://cardgame-player.herokuapp.com/' : 'http://localhost:8000';
-const socketServer =
-	process.env.NODE_ENV === 'production' ? 'wss://cardgame-player.herokuapp.com/' : 'ws://localhost:8000';
+const socketServer = process.env.NODE_ENV === 'production' ? 'cardgame-player.herokuapp.com/' : 'localhost:8000';
 
 const lobbyClient = new LobbyClient({ server });
 const client = new Client({
